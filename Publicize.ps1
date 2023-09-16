fe076e32ad952682ff9f6b036ab4d7440a8e5d75
Sylvain Corsini <sylvain.corsini@protonmail.com>
2023-09-16 14:47:31 +0200

    feat: add Publicize PS script

---
+++ Publicize.ps1
@@ -0,0 +1,21 @@
+﻿$timberbornDir = ".\Packages\Timberborn"
+$timberbornDlls = Get-ChildItem $timberbornDir -Filter "Timberborn.*.dll" -Recurse
+
+# Create a backup of the original dlls
+
+$timberbornDlls | ForEach-Object {
+    $backupPath = $_.FullName + ".bak"
+    Copy-Item $_.FullName $backupPath
+}
+
+# Publicize the dlls
+
+assembly-publicizer --overwrite --publicize-compiler-generated $($timberbornDlls | ForEach-Object { $_.FullName } )
+
+# Restore the original dlls
+
+#$timberbornDlls | ForEach-Object {
+#    $backupPath = $_.FullName + ".bak"
+#    Copy-Item $backupPath $_.FullName
+#    Remove-Item $backupPath
+#}
